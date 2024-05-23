2f29523beb6b399c73b146c007336d53bd0259e9
Samuel Torres <rilla@Samuels-iMac.local>
2024-05-22 20:16:53 -0400

    EOD

--- src/api/blog-article/controllers/blog-article.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * blog-article controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::blog-article.blog-article');
